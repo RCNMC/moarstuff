3895db3fe1710f6fecd4a9726b1af320d9e79af1
zacklogan <bigred9921@mchsi.com>
2016-07-05 10:01:37 -0500

    Create randomspawn.lua

---
+++ randomspawn.lua
@@ -0,0 +1,39 @@
+local function spawn( ply )
+	if (!ply.BuildMode) then
+	  spawnpoint = math.random(1, 15)
+	  if spawnpoint == 1 then
+	    ply:SetPos( )
+	  else if spawnpoint == 2 then
+	     ply:SetPos( )
+	  else if spawnpoint == 3 then
+	     ply:SetPos( )
+	  else if spawnpoint == 4 then
+	     ply:SetPos( )
+	  else if spawnpoint == 5 then
+	     ply:SetPos( )
+	  else if spawnpoint == 6 then
+	     ply:SetPos( )
+	  else if spawnpoint == 7 then
+	     ply:SetPos( )
+	  else if spawnpoint == 8 then
+	     ply:SetPos( )
+	  else if spawnpoint == 9 then
+	     ply:SetPos( )
+	  else if spawnpoint == 10 then
+	     ply:SetPos( )
+	  else if spawnpoint == 11 then
+	     ply:SetPos( )
+	  else if spawnpoint == 12 then
+	     ply:SetPos( )
+	  else if spawnpoint == 13 then
+	     ply:SetPos( )
+	  else if spawnpoint == 14 then
+	     ply:SetPos( )
+	  else
+	     ply:SetPos( )
+	  end
+	  -- Give weapons
+	else
+	  -- Current build mode code
+	end
+end
